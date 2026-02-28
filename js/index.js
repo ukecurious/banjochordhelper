8c91eca1bde54382fbb01389846e7715681cc037
ukecurious <ukecurious@gmail.com>
2026-02-28 18:26:06 +0000

    Updates

---
+++ js/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BanjoChordChart from "./BanjoChordChart";
+ReactDOM.render(/*#__PURE__*/React.createElement(BanjoChordChart, null), document.getElementById("root"));
